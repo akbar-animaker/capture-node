e254b3d1bfa56f4a470003e18f2a91b83750f683
Saravana <tsaravanakumar@animaker.com>
2020-09-25 15:35:19 +0530

    Pause and Resume

--- example.js
+++ example.js
@@ -6,7 +6,13 @@ async function main() {
   const recorder = aperture();
   recorder.startRecording();
   console.log('Recording started');
-  await delay(17000);
+  await delay(7000);
+  recorder.pauseRecording();
+  console.log('Recording Paused');
+  await delay(10000);
+  recorder.resumeRecording();
+  console.log('Recording Resumed');
+  await delay(10000);
   recorder.stopRecording();
   console.log('Video saved in the current directory');
 }
